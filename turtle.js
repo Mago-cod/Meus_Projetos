99911539968e050cee0d48480e51068ab7b4ba3b
Gui_mendes <victorm3ndes33@gmail.com>
2025-05-02 22:12:36 -0300

    Add files via upload
    Modificações na interatividade do site e no CSS e HTML

--- turtle.js
+++ turtle.js
@@ -1,8 +1,23 @@
-const btn=
-document.getElementById("btn-menu");
-    const sidebar=
-    document.getElementById("sidebar");
+const btnMenu=
+document.getElementById('btn-menu');
+    const navMenu=
+    document.querySelector('nav');
 
-btn.addEventListener("click",() => {
-    sidebar.classList.toggle("aberto");
-})
+    //Alterna a visibilidade do menu ao clicar no botão
+
+btnMenu.addEventListener("click",() => {
+    navMenu.classList.toggle('ativo');
+});
+
+    //Fecha o menu ao clicar na tela fora
+
+document.addEventListener('click',(Event) => 
+{
+    const clicaFora = !
+    navMenu.contains(Event.target) && !
+    btnMenu.contains(Event.target);
+        if(clicaFora) {
+            navMenu.classList.remove('ativo');
+        }
+});
+
